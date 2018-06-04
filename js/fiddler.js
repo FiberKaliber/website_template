4703a1cd73ebe6f02f4ef78e7689337ddf30b4f2
FiberKaliber <d.gunbrant@gmail.com>
2018-06-04 15:22:02 +0200

    almost working navbar

--- js/fiddler.js
+++ js/fiddler.js
@@ -1,7 +1,18 @@
-var dropdownMenu = document.getElementById("dropdown-menu");
+var navbar = document.getElementById('navbar');
+    style = window.getComputedStyle(navbar);
+    display = style.getPropertyValue('display');
 
+var dropDownButton = document.getElementById('mobile-dropdown-link');
+    styleMobile = window.getComputedStyle(dropDownButton);
+    visibility = styleMobile.getPropertyValue('visibility');
 
 function showDropdownMenu() {
-    console.log("hej");
-    //dropdownMenu.style.display
+    
+    if (display === 'none') {
+        display = 'block';
+    } else if (display === 'block') {
+        display = 'none';
+    }
+    console.log(display);
+    navbar.style.setProperty('display', display);
 }
