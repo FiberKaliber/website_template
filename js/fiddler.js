5cbde083cf8972263784a4214ed93846b5179779
David Gunbrant <david@webking.se>
2018-06-21 15:27:25 +0200

    background image and dropdown almost ready, only background color that needs som jsFiddle

--- js/fiddler.js
+++ js/fiddler.js
@@ -4,17 +4,20 @@
 
 var dropdownMenuParent = document.getElementById('dropdown-menu-parent');
 var dropdownMenu = document.getElementById('dropdown-menu');    
+var headerSize = document.getElementById('header')
 
 // Onclick on .mobile-dropdown-link inside #dropdownMenuparent toggles visible/hidden on menu
 dropdownMenuParent.onclick = function() {
         dropdownMenu.classList.toggle('dropdown-menu-visible');
         dropdownMenu.classList.toggle('dropdown-menu-hidden');
+        headerSize.classList.toggle('header-height-toggle');
 }
 
 // Restores default settings on bigger screens/devices
 window.addEventListener('resize', function() {
     if(window.innerWidth > 1024) {
-        dropdownMenu.classList.remove('dropdown-menu-visible');
         dropdownMenu.classList.add('dropdown-menu-hidden');
+        dropdownMenu.classList.remove('dropdown-menu-visible');
+        headerSize.classList.remove('header-height-toggle');
     }
 }); 
