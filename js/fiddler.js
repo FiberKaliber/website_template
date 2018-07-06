6c9812d638d858a63db7e3c57daaccc02d44ab05
David Gunbrant <david@webking.se>
2018-07-06 22:59:04 +0200

    h1 on home-page-title now has animation

--- js/fiddler.js
+++ js/fiddler.js
@@ -1,11 +1,34 @@
-/*========================== 
-        Dropdown menu
-==============================*/
-
 var dropdownMenuParent = document.getElementById('dropdown-div');
-var dropdownMenu = document.getElementById('sidebar');    
+var dropdownMenu = document.getElementById('sidebar');   
 
-// Onclick on .mobile-dropdown-link inside #dropdownMenuparent toggles visible/hidden on menu
+/*  Onclick on dropdown-div 
+    toggles visible/hidden on sidebar-menu */
 dropdownMenuParent.onclick = function() {
     dropdownMenu.classList.toggle('sidebar-visible');
 }
+
+/*   */
+function titleAnimation() {
+    var homePageTitle = document.getElementById('home-page-title');
+    var titleCharArray = homePageTitle.innerHTML.split("");
+    var titleTemp = "";
+    homePageTitle.innerHTML = ""; //empty h1 title inside #home-page-title
+    
+    (function theLoop (i) {
+        setTimeout(function () {
+          if (i < titleCharArray.length) {  
+            titleTemp += titleCharArray[i];
+            homePageTitle.innerHTML = titleTemp;
+            i++;    
+            theLoop(i);       
+          }
+        }, Math.floor((Math.random() * 400) + 100));
+      })(0);
+}
+
+window.onload = function() {
+    titleAnimation();
+
+    
+}
+
